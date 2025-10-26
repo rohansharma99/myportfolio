938f7bde1526d9be95834e2feae43a4b693b37b3
rohansharma99 <rohansharmaji9411@gmail.com>
2025-10-26 18:25:47 +0530

    deploy build

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -62,8 +62,8 @@ export const ProjectsSection = () => {
 
               <div className="p-6">
                 <div className="flex flex-wrap gap-2 mb-4">
-                  {project.tags.map((tag) => (
-                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                  {project.tags.map((tag ,id) => (
+                    <span key ={id} className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
                       {tag}
                     </span>
                   ))}
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //  base: "/beautiful-react-tailwind-portfolio/",
+   base: '/myportfolio',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
